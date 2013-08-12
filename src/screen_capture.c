312820583c707bb1f2442be102f080f9f43c63e2
qyocimonkey <qyocimonkey@Ubuntu-PC>
2013-08-12 14:15:14 +0800

    Add build.py

--- src/screen_capture.c
+++ src/screen_capture.c
@@ -32,9 +32,9 @@ int main()
 */
 
 #include <X11/Xlib.h>
+#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main(int argc, char **argv)
 {
@@ -43,10 +43,16 @@ int main(int argc, char **argv)
     int width = DisplayWidth(display,screen_num), height = DisplayHeight(display,screen_num);
 
     Window window = XCreateSimpleWindow(display, RootWindow(display, 0), 0, 0, width, height, 1, 0, 0);
-    double start = time();
+/*
+    struct timeval start,end;
+    gettimeofday(&start,NULL);
+*/
     XImage *ximage = XGetImage(display,RootWindow(display,screen_num),0,0,width,height,XAllPlanes(),ZPixmap);
-    double end = time();
-    fprintf(stdout,"%lf\n",end-start);
+/*
+    gettimeofday(&end,NULL);
+    int timeuse = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    fprintf(stdout,"%d\n",timeuse);
+  */  
     XSelectInput(display, window, ButtonPressMask | ExposureMask);
     XMapWindow(display, window);
     while(1)
